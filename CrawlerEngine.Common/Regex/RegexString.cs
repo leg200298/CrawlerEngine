e0edea4a957575972761b35a135156f5b1f4cb6f
Tom <tm731531@hotmail.com>
2020-07-07 00:59:49 +0800

    add regex

---
+++ CrawlerEngine.Common/Regex/RegexString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerEngine.Common
+{
+    public class RegexString
+    {
+        public string isPrice = @"\d+";
+        public string isPriceMatch = @"\D{1,3}(價|價格)\${0,1}\d+,{0,1}\d+";
+        public string isXZeroFee = @"\d{1,2}期0利率";
+        public string nBank = @"\d{1,2}家";
+        public string isATM = @"ATM";
+        public string getPayment = @"\D{2}(付款|付清|分期)|\D{0,10}Pay|\D{2}卡|\D{2}支付|ibon|FamiPort";
+        public string isBank = @"\D{2,4}銀行\D{0,5}";
+        public string splitPay = @"每期\d+元";
+        public string getWarranty = @"\d{1,2}(年|月)保(固|固期)";
+        public string getSplitPayPer = @"\${0,1}\d{0,3},{0,1}\d{0,3},{0,1}\d+\/(期)";
+    }
+}
