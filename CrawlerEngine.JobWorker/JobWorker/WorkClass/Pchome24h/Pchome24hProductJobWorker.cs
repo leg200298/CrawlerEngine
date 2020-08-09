e9a3f5968260f060f63f0ff8bf30792de1eb1521
Tom <tm731531@hotmail.com>
2020-08-09 22:54:41 +0800

    更新 pchome24h product 的cookie

--- CrawlerEngine.JobWorker/JobWorker/WorkClass/Pchome24h/Pchome24hProductJobWorker.cs
+++ CrawlerEngine.JobWorker/JobWorker/WorkClass/Pchome24h/Pchome24hProductJobWorker.cs
@@ -56,8 +56,13 @@ namespace CrawlerEngine.JobWorker.WorkClass
             //httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36");
             //httpClient.DefaultRequestHeaders.Add("Referer", jobInfo.Url);
 
-            httpClient.DefaultRequestHeaders.Add("User-Agent", "Googlebot");
-            httpClient.DefaultRequestHeaders.Add("Referer", "https://www.google.com");
+            //httpClient.DefaultRequestHeaders.Add("User-Agent", "Googlebot");
+
+            //httpClient.DefaultRequestHeaders.Add("Referer", "https://www.google.com");
+
+            httpClient.DefaultRequestHeaders.Add("if-modified-since", "Wed, 05 Aug 2020 07:19:40 GMT");
+            httpClient.DefaultRequestHeaders.Add("cookie", "ECC=b8e15585a23f4bd600b19d46374059235d83cdde.1485782368; E=zRHrOYjNV72DcEKG9ysZXv02CZCJy%2FLy0pPR5Y5ZaIYAYfMKaXQT9aOd1J%2FKxHPE6rY9TAqtAyXBLcZXVkHJQihtCOoSxBHF; ECWEBSESS=fd2cc93926.b31bebc8cad0786c92aec522b560552eb3cb28b3.1509933443; venguid=bab70cd8-c8c6-45ed-a4e4-41ac4e98ed40.instance-group-venapi-fn0t20181216; puuid=K.20190428212238.72; uuid=0d710e9c-f407-4322-921a-7e4d056b1c3e; _gcl_au=1.1.1863783388.1594038055; _fbp=fb.2.1594038056459.2066246521; U=a6ab26ff126bde983e390a48d2548d5aab1f196a; PCHOMEUNIQID=041d156e5b4c7cb1fc71562154c6684b; _pahc_t=1594312694; CID=ae193f7bde466bb822d4810eb053a02e1d3355d1; X=8417702; MBR=tm731531%40hotmail.com; gsite=24h; PRI_tm731531%40hotmail.com=0; HistoryEC=%7B%22P%22%3A%5B%7B%22Id%22%3A%22DSAA31-A900A6PB5%22%2C%20%22M%22%3A1596981729%7D%2C%20%7B%22Id%22%3A%22DJAA2V-A900AM97O%22%2C%20%22M%22%3A1596882835%7D%5D%2C%20%22T%22%3A1%7D; _gid=GA1.3.770563525.1596981731; vensession=9259417d-3cd8-4c5b-b07d-d8038b6783f2.wg1-1n4020200809.se; _ga=GA1.1.1839673043.1529073435; _ga_9CE1X6J1FG=GS1.1.1596981729.13.0.1596981756.0");
+
 
             return httpClient;
         }
